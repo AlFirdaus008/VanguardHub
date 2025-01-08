8bd6e2f02bcf0a7b8d9978111ff0075abcba3b65
Nama Anda <24031554008@mhs.unesa.ac.id>
2025-01-08 11:41:37 +0700

    new web

--- webb/scheduling.py
+++ webb/scheduling.py
@@ -548,7 +548,8 @@ def get_month_with_most_events(numid, file_path_users, time_min='2024-01-01T00:0
         singleEvents=True, 
         orderBy='startTime' 
     ).execute().get('items', []) 
- 
+    calendarId=get_calendar_id_from_csv(file_path_users, numid)
+    print(calendarId)
     if not events: 
         raise Exception("No events found.") 
  
